9a9dcb93f2117637b148e68ef34a7d6896e98c0a
jenkins <jenkins@teamw.in>
2016-01-30 15:18:51 -0500

    updated v1awifi kernel P900UEU1COF4

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Jan 29 18:44:02 EST 2016"
+#define LINUX_COMPILE_BY "jenkins"
+#define LINUX_COMPILE_HOST "build"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
